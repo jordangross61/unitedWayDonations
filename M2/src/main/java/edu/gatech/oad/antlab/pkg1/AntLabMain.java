26657f0381962ce5305119452978b06642b91d9a
Shilpa Patel <shilpapatel1123@gmail.com>
2018-09-06 12:09:24 -0400

    Shilpas antlab changes

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("mbrickell3");
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("gburdell5");
+          Person5 p5 = new Person5("Shilpa Patel");
+          toPrint += p5.toString("spatel626");
           //Person6 replace P6 with your name
           //and gburdell5 with your gt id
           Person5 p6 = new Person6("Krishna Patel");
