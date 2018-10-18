ce9e852d9f84a7d2d9d0d7ac903868ef63a54790
[patelkb99] <Krishna@Krishnas-MacBook-Pro.local>
2018-10-18 00:11:57 -0400

    different layout for with or without size

--- buzztracker/app/src/main/java/edu/gatech/cs2340/buzzTracker/controllers/AddCategoryActivity.java
+++ buzztracker/app/src/main/java/edu/gatech/cs2340/buzzTracker/controllers/AddCategoryActivity.java
@@ -3,11 +3,13 @@ package edu.gatech.cs2340.buzzTracker.controllers;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import edu.gatech.cs2340.buzzTracker.R;
+import edu.gatech.cs2340.buzzTracker.model.Item;
 import edu.gatech.cs2340.buzzTracker.model.ItemType;
 import edu.gatech.cs2340.buzzTracker.model.UserRights;
 
@@ -23,7 +25,7 @@ public class AddCategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
 
-        categorySpinner = findViewById(R.id.access_spinner);
+        categorySpinner = findViewById(R.id.category_spinner);
 
         ArrayAdapter<UserRights> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, ItemType.values());
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,6 +33,12 @@ public class AddCategoryActivity extends AppCompatActivity {
     }
 
     public void onAddCategoryPressed(View view){
-        startActivity(new Intent(this, AddItemActivity.class));
+        ItemType selected = (ItemType)categorySpinner.getSelectedItem();
+        // if item being added would need a size
+        if (selected.equals(ItemType.CLOTHING)) {
+            startActivity(new Intent(this, AddSizeItemActivity.class));
+        } else {
+            startActivity(new Intent(this, AddItemActivity.class));
+        }
     }
 }
--- buzztracker/app/src/main/java/edu/gatech/cs2340/buzzTracker/controllers/AddSizeItemActivity.java
+++ buzztracker/app/src/main/java/edu/gatech/cs2340/buzzTracker/controllers/AddSizeItemActivity.java
@@ -4,33 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import edu.gatech.cs2340.buzzTracker.R;
+import edu.gatech.cs2340.buzzTracker.model.ItemType;
+import edu.gatech.cs2340.buzzTracker.model.Size;
+import edu.gatech.cs2340.buzzTracker.model.UserRights;
 
 /**
  * Controller for the add item view of application
  */
-public class AddItemActivity extends AppCompatActivity {
+public class AddSizeItemActivity extends AppCompatActivity {
 
     private EditText shortField;
     private EditText longField;
     private EditText valueField;
     private EditText commentField;
+    private Spinner sizeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_add_item_with_size);
 
         shortField = findViewById(R.id.short_field);
         longField = findViewById(R.id.long_field);
         valueField = findViewById(R.id.value_field);
         commentField = findViewById(R.id.comments_field);
+        sizeSpinner = findViewById(R.id.size_spinner);
+
+        ArrayAdapter<UserRights> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Size.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sizeSpinner.setAdapter(adapter);
     }
 
     public void onAddItemPressed(View view){
-        startActivity(new Intent(this, DashboardActivityEmployee.class));
-    }
+        startActivity(new Intent(this, DashboardActivityEmployee.class));    }
 
 }
