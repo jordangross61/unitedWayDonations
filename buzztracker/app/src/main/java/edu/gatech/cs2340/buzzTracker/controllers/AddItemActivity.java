9f8a38383206725794835b304881224b2634a478
[patelkb99] <Krishna@Krishnas-MacBook-Pro.local>
2018-10-17 23:34:08 -0400

    updated add item UI

--- buzztracker/app/src/main/java/edu/gatech/cs2340/buzzTracker/controllers/AddItemActivity.java
+++ buzztracker/app/src/main/java/edu/gatech/cs2340/buzzTracker/controllers/AddItemActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import edu.gatech.cs2340.buzzTracker.R;
 
@@ -12,10 +13,20 @@ import edu.gatech.cs2340.buzzTracker.R;
  */
 public class AddItemActivity extends AppCompatActivity {
 
+    private EditText shortField;
+    private EditText longField;
+    private EditText valueField;
+    private EditText commentField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        shortField = findViewById(R.id.short_field);
+        longField = findViewById(R.id.long_field);
+        valueField = findViewById(R.id.value_field);
+        commentField = findViewById(R.id.comments_field);
     }
 
 }
